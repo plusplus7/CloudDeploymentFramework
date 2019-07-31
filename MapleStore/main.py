807f5a69aa2e2f58a7d6a0507e3bd7633c2da8cf
plusplus7 <jackiedeng.cn@gmail.com>
2019-07-31 20:31:22 +0800

    Update main.py

--- MapleStore/main.py
+++ MapleStore/main.py
@@ -3,27 +3,79 @@ from flask import request
 from flask import Flask
 import mysql.connector
 import json
+import datetime
 import hashlib
 
 FlaskApp = Flask(__name__, template_folder='templates')
-FlaskApp.debug = False
+FlaskApp.debug = True
 FlaskApp.secret_key = 'development'
 
-@FlaskApp.route('/')
+auth_fail_callback = "https://plusplus7.com/storage/maplestore"
+
+def validate_token(token):
+    if token == None:
+        return ('Anonymous', False)
+
+    tokens = token.split(":")
+
+    if len(tokens) != 5:
+        return ('Anonymous', False)
+
+    date = tokens[0]
+    curdate = datetime.datetime.now().strftime('%Y-%m-%d')
+    if date != curdate:
+        return ('Anonymous', False)
+
+    user = tokens[1]
+    data = tokens[2]
+    content = f"{date}:{user}:{data}"
+
+    signature = hmac.new(FlaskApp.secret_key, msg=content.encode("ascii"), digestmod=hashlib.sha256).hexdigest()
+    if tokens[3] == signature:
+        return (user, True)
+
+    return (f'Anonymous-{user}', False)
+
+@FlaskApp.route('/', methods=['GET', 'POST'])
 def Homepage():
-    return flask.render_template('pages/home.html')
+    if request.method == 'POST':
+        token = request.args.get('token')
+        (username, isValidToken) = validate_token(token)
+        if isValidToken == False:
+            flask.redirect(auth_fail_callback)
+
+        resp = make_response(flask.render_template('pages/home.html', username = username))
+        resp.set_cookie('token', token)
+        return resp
+
+    else:
+        (username, isValidToken) = validate_token(request.cookies.get('token'))
+        if isValidToken == False:
+            return flask.redirect(auth_fail_callback)
+        return flask.render_template('pages/home.html')
 
 @FlaskApp.route('/clientinfo')
 def ClientInfoPage():
+    (username, isValidToken) = validate_token(request.cookies.get('token'))
+    if isValidToken == False:
+        return flask.redirect(auth_fail_callback)
+
     return flask.render_template('pages/clientinfo.html')
 
 @FlaskApp.route('/accounts')
 def AccountsPage():
+    (username, isValidToken) = validate_token(request.cookies.get('token'))
+    if isValidToken == False:
+        return flask.redirect(auth_fail_callback)
+
     (conn, cur) = connect()
     return flask.render_template('pages/accounts.html')
 
 @FlaskApp.route('/accounts/create')
 def CreateAccountPage():
+    (username, isValidToken) = validate_token(request.cookies.get('token'))
+    if isValidToken == False:
+        return flask.redirect(auth_fail_callback)
 
     (conn, cur) = connect()
 
@@ -37,6 +89,10 @@ def CreateAccountPage():
 
 @FlaskApp.route('/giveaway')
 def GiveawayPage():
+    (username, isValidToken) = validate_token(request.cookies.get('token'))
+    if isValidToken == False:
+        return flask.redirect(auth_fail_callback)
+
     (conn, cur) = connect()
     return flask.render_template('pages/giveaway.html')
 
@@ -74,10 +130,14 @@ def connect():
     return (mydb, mycursor)
     
 if __name__ == "__main__":
-    from cherrypy  import wsgiserver
+    FlaskApp.run()
+
+"""    from cherrypy  import wsgiserver
         
     server = wsgiserver.CherryPyWSGIServer(('0.0.0.0', 8080), FlaskApp)
     try:
         server.start()
     except KeyboardInterrupt:
         server.stop()
+
+"""
